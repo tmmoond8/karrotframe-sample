ffb19f3651939b075c1f3d1edbba99d3d069e6fc
tmmoond8 <tmmoond8@gmail.com>
2022-02-06 12:29:44 +0900

    🚧 karrotframe navigation 적용

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Search } from "./Search";
+export { default as Evaluate } from "./Evaluate";
+export { default as MyPage } from "./MyPage";
